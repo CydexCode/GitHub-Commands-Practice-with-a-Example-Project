570b0ada6855dbbdc407e01098deec6dfab669d7
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-10-11 11:14:47 +0530

    home page design

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -4,21 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Welcome to the Home Page</h1>
+      <p>
+        This is the home page of your React application.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
   );
 }
 
